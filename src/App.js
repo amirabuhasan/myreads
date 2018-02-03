fde82092f45239b03dc0fe1247590da22d86f206
amirabuhasan <amirabuhasan@gmail.com>
2018-02-03 15:38:16 +0800

    Separated CurrentlyReading, WantToRead, and Read into separate components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Route } from "react-router-dom"
 import Search from "./search"
 import BookList from "./BookList"
+import CurrentlyReading from "./CurrentlyReading"
+import Read from "./Read"
+import WantToRead from "./WantToRead"
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
   }
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+    BooksAPI.getAll().then((wantToRead) => {
+      this.setState({ wantToRead })
     })
   }
 
@@ -20,12 +26,33 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookList />
-        )}/>
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+              <div>
+                <WantToRead
+                books={this.state.wantToRead}/>
+                <CurrentlyReading
+                books={this.state.currentlyReading}/>
+                <Read
+                books={this.state.read}/>
+              </div>
+          </div>
+          <div className="open-search">
+            <Link
+              to="/search"
+              >Add a book
+            </Link>
+          </div>
+        </div>
+          )}/>
         <Route path="/search" render={() => (
           <Search />
         )}/>
-      </div>
+    </div>
+
     )
   }
 }
